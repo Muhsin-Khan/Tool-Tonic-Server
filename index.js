9ed28375f153bc4d9ff7330efeb953b827fa8aae
Muhsin Khan <mdmuhsinuddin01@gmail.com>
2022-05-28 20:24:12 +0600

    manage all products api

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
       res.send(product);
     });
 
+     // Managing All Products the product.....
+     app.delete('/product/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.post("/order", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
